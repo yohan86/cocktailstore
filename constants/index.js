fdf7ea22e9449549669ca28237bbce3bd6b7af2c
Yohan <yohanlahiru86@gmail.com>
2025-06-25 17:32:12 +0530

    added data

--- constants/index.js
+++ constants/index.js
@@ -15,6 +15,60 @@ const navLinks = [
        id: "contact",
        title: "Contact",
     },
-   ];
+];
 
-   export {navLinks};
+const cocktailLists = [
+    {
+        name: "Chapel Hill Shiraz",
+        country: "AU",
+        detail: "Battle",
+        price: "$10",
+    },
+    {
+        name: "Caten Malbee",
+        country: "AU",
+        detail: "Battle",
+        price: "$49",
+    },
+    {
+        name: "Rhino Pale Ale",
+        country: "CA",
+        detail: "750 ml",
+        price: "$20",
+    },
+    {
+        name: "Irish Guinness",
+        country: "IE",
+        detail: "600 ml",
+        price: "$29",
+    },
+];
+
+const mockTailLists = [
+    {
+        name: "Tropical Bloom",
+        country: "US",
+        detail: "Battle",
+        price: "$10",
+    },
+    {
+        name: "Passionfruit Mint",
+        country: "US",
+        detail: "Battle",
+        price: "$49",
+    },
+    {
+        name: "Citrus Glow",
+        country: "CA",
+        detail: "750 ml",
+        price: "$20",
+    },
+    {
+        name: "Lavender Fizz",
+        country: "IE",
+        detail: "600 ml",
+        price: "$29",
+    },
+];
+
+   export {navLinks, cocktailLists, mockTailLists};
